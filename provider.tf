dc873bed6659b121105af726b224eec77d72f05a
rahulmahure <rahulmahure3@gmail.com>
2026-09-08 10:21:33 +0530

    Identity Roles

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 provider "aws" {
   region = "ap-south-1"
 
-
+  default_tags {
+    tags = {
+      Project     = "https://github.com/rahulmahure/aws-iam-identity-roles.git"
+      ManagedBy   = "CloudOps"
+    }
+  }
 }
