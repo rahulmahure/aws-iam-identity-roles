5bdb7805b50595847dc853c68fca81a2356955fd
rahulmahure <rahulmahure3@gmail.com>
2026-09-09 22:43:55 +0530

    Adding Roles

--- modules/iam-role/main.tf
+++ modules/iam-role/main.tf
@@ -7,23 +7,23 @@ resource "aws_iam_role" "this" {
 
 # Existing AWS Managed policies
 resource "aws_iam_role_policy_attachment" "managed" {
-  for_each = toset(var.policy_arns)
+  for_each   = toset(var.policy_arns)
   role       = aws_iam_role.this.name
   policy_arn = each.value
 }
 
 # Create customer-managed policies
 resource "aws_iam_policy" "custom_policies" {
-  for_each = var.policy_documents_json
+  for_each    = var.policy_documents_json
   name        = "${var.name}-${each.key}"
   description = "Customer managed policy for ${var.name}"
-  policy = each.value
-  tags = var.tags
+  policy      = each.value
+  tags        = var.tags
 }
 
 # Attach customer-managed policies
 resource "aws_iam_role_policy_attachment" "custom_policies_attachment" {
-  for_each = aws_iam_policy.custom_policies
+  for_each   = aws_iam_policy.custom_policies
   policy_arn = each.value.arn
   role       = aws_iam_role.this.name
 }
